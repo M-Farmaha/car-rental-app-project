faf195d912ce9a2382d7e60afec5f325b869e67a
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-09-17 19:54:36 +0300

    Updates

--- src/components/ButtonText/ButtonText.jsx
+++
@@ -1,9 +0,0 @@
-import { ButtonWrap } from "./ButtonText-styles";
-
-export const ButtonText = ({ children, onClick, type, sx }) => {
-  return (
-    <ButtonWrap type={type} onClick={onClick} sx={sx}>
-      {children}
-    </ButtonWrap>
-  );
-};
