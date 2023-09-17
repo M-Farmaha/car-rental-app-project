8f2ab7dc1f09bc0c97c0fa97e82b595a2f9049c1
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-09-17 20:19:00 +0300

    added basename

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/car-rental-app-project">
       <App />
     </BrowserRouter>
   </React.StrictMode>
